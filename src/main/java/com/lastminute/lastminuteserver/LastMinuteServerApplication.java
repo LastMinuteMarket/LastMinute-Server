04115448707eb4a9a40913ab8f5cb3bb824ae38a
김형기 <khk211113@naver.com>
2023-05-31 14:36:47 +0900

    chore: 프로젝트 설정

---
+++ src/main/java/com/lastminute/lastminuteserver/LastMinuteServerApplication.java
@@ -0,0 +1,13 @@
+package com.lastminute.lastminuteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastMinuteServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastMinuteServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lastminute/lastminuteserver/LastMinuteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastminute.lastminuteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastMinuteServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
