fb380fb6485f6a1a756cd0082d234afbbb3afc74
김형기 <khk211113@naver.com>
2023-06-03 17:45:29 +0900

    refactor: Convertable의 getKey() 메서드 이름 변경(혼동 방지)

--- src/main/java/com/lastminute/lastminuteserver/payment/dto/PaymentDto.java
+++ src/main/java/com/lastminute/lastminuteserver/payment/dto/PaymentDto.java
@@ -1,24 +1,38 @@
 package com.lastminute.lastminuteserver.payment.dto;
 
+import com.lastminute.lastminuteserver.common.EnumValid;
+import com.lastminute.lastminuteserver.payment.domain.InstallmentPeriod;
 import com.lastminute.lastminuteserver.payment.domain.Payment;
+import com.lastminute.lastminuteserver.payment.domain.PaymentMethod;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
 
 @Builder
 public record PaymentDto(
-    String paymentMethod,
-    Integer installmentPeriod,
+    @NotNull
+    @EnumValid(enumClass = PaymentMethod.class, message = "지원하지 않는 결제 방식입니다.")
+    PaymentMethod paymentMethod,
+
+    @NotNull
+    @EnumValid(enumClass = InstallmentPeriod.class, message = "지원하지 않는 기간입니다.")
+    InstallmentPeriod installmentPeriod,
+
     Integer originalPrice,
+
     Integer fee,
+
     Integer finalPrice,
+
     LocalDateTime acceptedAt,
+
     Boolean cancelAvailable
 ) {
     public static PaymentDto of(Payment entity){
         return PaymentDto.builder()
-                .paymentMethod(entity.getPaymentMethod().getKey())
-                .installmentPeriod(entity.getInstallmentPeriod().getKey())
+                .paymentMethod(entity.getPaymentMethod())
+                .installmentPeriod(entity.getInstallmentPeriod())
                 .originalPrice(entity.getOriginalPrice())
                 .fee(entity.getFee())
                 .finalPrice(entity.getFinalPrice())
