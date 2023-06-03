e516e072c978a623d20a0e68153a0ade4a6fbcd2
김형기 <khk211113@naver.com>
2023-06-03 20:39:08 +0900

    feat: 현재위치 및 시간 기반 물품 검색 쿼리 추가

--- src/main/java/com/lastminute/lastminuteserver/product/repository/ProductRepository.java
+++ src/main/java/com/lastminute/lastminuteserver/product/repository/ProductRepository.java
@@ -2,8 +2,37 @@ package com.lastminute.lastminuteserver.product.repository;
 
 
 import com.lastminute.lastminuteserver.product.domain.Product;
+import com.lastminute.lastminuteserver.product.domain.ProductState;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.geo.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query(value = """
+            SELECT pd
+            FROM Product pd
+            JOIN FETCH User wr
+            LEFT JOIN FETCH Placement pl
+            WHERE FUNCTION('ST_Distance_Sphere', pl.location, :position) < :meters
+            AND pd.reservedTime >= FUNCTION('NOW')
+            AND pd.productState = :state
+            """)
+    Slice<Product> searchByLocation(Point position, ProductState state, Integer meters, Pageable pageable);
+
+    @Query(value = """
+            SELECT pd
+            FROM Product pd
+            JOIN FETCH User wr
+            LEFT JOIN FETCH Placement pl
+            WHERE FUNCTION('ST_Distance_Sphere', pl.location, :position) < :meters
+            AND pd.reservedTime BETWEEN :startTime AND :endTime
+            AND pd.productState = :state
+            """)
+    Slice<Product> searchByLocationAndTime(Point position, ProductState state, Integer meters, LocalDateTime startTime, LocalDateTime endTime, Pageable pageable);
 
 }
