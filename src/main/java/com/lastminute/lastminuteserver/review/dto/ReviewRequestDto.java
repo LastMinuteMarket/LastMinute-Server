db8ba19afdcee4332c7aeea9b7dba26deb064bbd
jycforest29 <jycforest29.dev@gmail.com>
2023-06-03 18:45:17 +0900

    feat: review dto 추가

--- src/main/java/com/lastminute/lastminuteserver/review/dto/ReviewRequestDto.java
+++ src/main/java/com/lastminute/lastminuteserver/review/dto/ReviewRequestDto.java
@@ -1,4 +1,10 @@
 package com.lastminute.lastminuteserver.review.dto;
 
+import jakarta.validation.constraints.Size;
+
 public class ReviewRequestDto {
+    @Size(min = 5, max = 100, message = "5~100 글자 사이로 입력해야 합니다.")
+    private String title;
+    @Size(min = 5, max = 1000, message = "5~1000 글자 사이로 입력해야 합니다.")
+    private String content;
 }
--- src/main/java/com/lastminute/lastminuteserver/review/dto/ReviewResponseDto.java
+++ src/main/java/com/lastminute/lastminuteserver/review/dto/ReviewResponseDto.java
@@ -1,4 +1,28 @@
 package com.lastminute.lastminuteserver.review.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class ReviewResponseDto {
+    private String title;
+
+    private String content;
+
+    private String userNickName;
+
+    private Long productId;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime createdAt;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime updatedAt;
 }
