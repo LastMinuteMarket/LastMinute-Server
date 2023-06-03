44dc55e8bd4f0c90a36bd1cbc06b3b3f49167c92
jycforest29 <jycforest29.dev@gmail.com>
2023-06-03 16:44:25 +0900

    feat: RequestException 전역 예외 핸들러 추가

--- src/main/java/com/lastminute/lastminuteserver/exceptions/RequestExceptionHandler.java
+++ src/main/java/com/lastminute/lastminuteserver/exceptions/RequestExceptionHandler.java
@@ -1,5 +1,16 @@
 package com.lastminute.lastminuteserver.exceptions;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class RequestExceptionHandler {
 
+    @ExceptionHandler(value = RequestException.class)
+    public ResponseEntity<Object> handleException(RequestException requestException){
+        RequestExceptionCode exceptionCode = requestException.getExceptionCode();
+        return ResponseEntity.status(exceptionCode.getHttpStatus())
+                .body(exceptionCode.getMessage());
+    }
 }
