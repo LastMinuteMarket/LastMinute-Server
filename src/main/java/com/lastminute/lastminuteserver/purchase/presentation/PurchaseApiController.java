fb14f609dc459008ae020fea132eb2940b5f8b09
jycforest29 <jycforest29.dev@gmail.com>
2023-06-04 11:47:15 +0900

    feat: purchase 컨트롤러 추가

---
+++ src/main/java/com/lastminute/lastminuteserver/purchase/presentation/PurchaseApiController.java
@@ -0,0 +1,47 @@
+package com.lastminute.lastminuteserver.purchase.presentation;
+
+import com.lastminute.lastminuteserver.purchase.dto.PurchaseCreateDto;
+import com.lastminute.lastminuteserver.purchase.dto.PurchaseResponseDto;
+import com.lastminute.lastminuteserver.purchase.service.PurchaseService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class PurchaseApiController {
+    private final PurchaseService purchaseService;
+
+    @PostMapping(value = "{productId}/purchase")
+    public ResponseEntity<PurchaseResponseDto> createPurchase(Long productId,
+                                                              Long userId,
+                                                              @Valid PurchaseCreateDto purchaseCreateDto){
+        PurchaseResponseDto purchaseResponseDto = purchaseService.createPurchase(productId, userId, purchaseCreateDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(purchaseResponseDto);
+    }
+
+    @GetMapping(value = "mypage/purchase/{purchaseId}")
+    public ResponseEntity<PurchaseResponseDto> getPurchase(Long purchaseId){
+        PurchaseResponseDto purchaseResponseDto = purchaseService.getPurchase(purchaseId);
+        return ResponseEntity.status(HttpStatus.OK).body(purchaseResponseDto);
+    }
+
+    @GetMapping(value = "mypage/purchase")
+    public ResponseEntity<List<PurchaseResponseDto>> getPurchaseListByUser(Long userId){
+        List<PurchaseResponseDto> purchaseResponseDto = purchaseService.getPurchaseListByUser(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(purchaseResponseDto);
+    }
+
+    @DeleteMapping(value = "mypage/purchase/{purchaseId}")
+    public ResponseEntity<Object> deletePurchase(Long purchaseId){
+        purchaseService.deletePurchase(purchaseId);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+}
